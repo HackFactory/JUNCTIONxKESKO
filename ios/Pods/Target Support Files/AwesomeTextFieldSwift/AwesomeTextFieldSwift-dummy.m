c9ac099e4da956c7b10d4a2a2e2021d7b24bee58
Yaroslav Spirin <yar.spirin@gmail.com>
2018-11-24 22:26:13 +0200

    add black login theme

---
+++ ios/Pods/Target Support Files/AwesomeTextFieldSwift/AwesomeTextFieldSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AwesomeTextFieldSwift : NSObject
+@end
+@implementation PodsDummy_AwesomeTextFieldSwift
+@end
---
+++ ios/build/Pods.build/Debug-iphoneos/AwesomeTextFieldSwift.build/Objects-normal/arm64/AwesomeTextFieldSwift-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/yar/Desktop/iOS\ Education/Projects/K\ Group/ios/Pods/Target\ Support\ Files/AwesomeTextFieldSwift/AwesomeTextFieldSwift-dummy.m \
+  /Users/yar/Desktop/iOS\ Education/Projects/K\ Group/ios/build/Pods.build/Debug-iphoneos/AwesomeTextFieldSwift.build/module.modulemap \
+  /Users/yar/Desktop/iOS\ Education/Projects/K\ Group/ios/Pods/Target\ Support\ Files/AwesomeTextFieldSwift/AwesomeTextFieldSwift-prefix.pch
